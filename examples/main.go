dbf3c9ec1b12f5187fff9ab40be6e0d50c6c1019
Berkay Ersoy <berkay.ersoy.007@gmail.com>
2024-08-21 12:43:17 +0300

    update main.go

--- examples/main.go
+++ examples/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	go_arabic_stemmer "go-arabic-stemmer/arabic/stemmer"
+	"go-arabic-light-stemmer/arabic/stemmer"
 )
 
 func main() {
-	arStemmer := go_arabic_stemmer.NewArabicLightStemmer()
+	arStemmer := stemmer.NewArabicLightStemmer()
 	stem := arStemmer.LightStem("أفتضاربانني")
 	fmt.Println("Stemmed word:", stem)
 }
